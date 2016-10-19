159a68b2df1b67b254a7666516e511b1fbc024d6
billbunkum <william.richard.hurst@gmail.com>
2016-10-19 17:19:07 -0400

    style calculator

--- app/forms.py
+++ app/forms.py
@@ -4,16 +4,16 @@ class WordCount(Form):
 	count = StringField("Enter Words to Count", validators=[validators.DataRequired()])
 
 class CalculatorForm(Form):
-	number1 = DecimalField("Enter first number", default=0.0, validators=[validators.DataRequired()])
-	number2 = DecimalField("Enter second number", default=0.0, validators=[validators.DataRequired()])
+	number1 = DecimalField("", default=0.0, validators=[validators.DataRequired()])
+	number2 = DecimalField("", default=0.0, validators=[validators.DataRequired()])
 	operator = RadioField("Operators",
 		default = "+ Add",
 		choices = [
-			("+ Add", "+ Add"),
-			("- Subtract", "- Subtract"),
-			("* Multiply", "* Multiply"),
-			("/ Divide", "/ Divide")
-		])
+			("+ Add", "+"),
+			("- Subtract", "-"),
+			("* Multiply", "*"),
+			("/ Divide", "/")
+		]) #the second one is what appears
 
 class RepoForm(Form):
 	username = StringField("Enter the Username of Interest", validators=[validators.DataRequired()])
